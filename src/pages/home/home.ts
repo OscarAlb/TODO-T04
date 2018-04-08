2a7225f0881fb152f2e264ef01cbed8d2da471c2
Oscar <oscar101019@gmail.com>
2018-04-08 15:36:01 -0600

    Funcion de agregar elementos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,34 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public todos=[];
 
+  constructor(public navCtrl: NavController, 
+    private alertController: AlertController) {
+
+  }
+
+  openToDoAlert() {
+    let addToDoAlert = this.alertController.create({
+      title: "Add A ToDo",
+      message: "Enter your todo",
+      inputs: [{
+        type: "text",
+        name: "addTodoInput"
+      }],
+      buttons: [{
+        text: "Cancel"
+      },
+      {
+        text: "Add ToDo",
+        handler:(inputData)=>{
+          let todoText;
+          todoText=inputData.addTodoInput;
+          this.todos.push(todoText);
+        }
+      }]
+    });
+    addToDoAlert.present()
   }
 
 }
